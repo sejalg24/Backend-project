87809fb52c10d9839304c079b41d5dd6cdea81d7
Dhiraj Kushwaha <dhirajkushwaha0714@gmail.com>
2025-07-18 22:44:23 +0530

    fixed my sillies

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,7 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-import dotenv from "dotenv";
-dotenv.config({
-    path:'./env'
-})
-
+ 
 
 const connectDB = async () => {
     try{
